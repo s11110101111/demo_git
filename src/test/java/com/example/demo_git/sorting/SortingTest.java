4687e22411613b554d73d0dabc328177a4776118
s11110101111 <s11110101111@gmail.com>
2022-07-25 11:17:45 +0500

    add pom for test

---
+++ src/test/java/com/example/demo_git/sorting/SortingTest.java
@@ -0,0 +1,131 @@
+package com.example.demo_git.sorting;
+
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ */
+@Slf4j
+public class SortingTest {
+
+
+
+ List<UserSortTest> getTestDealers() {
+        UserSortTest user = new UserSortTest("Иван", "Иванов", 106);
+        user.setAdress(new Adress("Пушкина", "Москва", "10"));
+        user.products.add(new Price("Горох", BigDecimal.TEN, 100));
+        user.products.add(new Price("Яблоко", BigDecimal.valueOf(10.22), 100));
+        user.products.add(new Price("Pickle", BigDecimal.valueOf(100.00), 100));
+        UserSortTest user1 = new UserSortTest("Петр", "Петров", 101);
+        user1.setAdress(new Adress("Горького", "Анапа", "101"));
+        user1.products.add(new Price("Горох", BigDecimal.TEN, 100));
+        user1.products.add(new Price("Яблоко", BigDecimal.valueOf(10.22), 100));
+        user1.products.add(new Price("Pickle", BigDecimal.valueOf(100.00), 100));
+        UserSortTest user2 = new UserSortTest("Иван", "Петров", 106);
+        user2.setAdress(new Adress("Маяковского", "Москва", "110"));
+        user2.products.add(new Price("Горох", BigDecimal.TEN, 100));
+        user2.products.add(new Price("Яблоко", BigDecimal.valueOf(10.22), 100));
+        user2.products.add(new Price("Pickle", BigDecimal.valueOf(100.00), 100));
+        user2.products.add(new Price("Банан", BigDecimal.valueOf(1.20), 0));
+        UserSortTest user3 = new UserSortTest("Семен", "Семенов", 102);
+        user3.setAdress(new Adress("Достоевского", "Анапа", "10"));
+        user3.products.add(new Price("Горох", BigDecimal.TEN, 100));
+        user3.products.add(new Price("Яблоко", BigDecimal.valueOf(10.22), 100));
+        user3.products.add(new Price("Pickle", BigDecimal.valueOf(100.00), 100));
+        user3.products.add(new Price("Банан", BigDecimal.valueOf(1.20), 0));
+        UserSortTest user4 = new UserSortTest("Chuck", "Norris", 105);
+        user4.setAdress(new Adress("Пушкина", "Москва", "10"));
+        user4.products.add(new Price("Горох", BigDecimal.TEN, 100));
+        user4.products.add(new Price("Яблоко", BigDecimal.valueOf(10.22), 100));
+        user4.products.add(new Price("Pickle", BigDecimal.valueOf(100.00), 100));
+        user4.products.add(new Price("Pickle", BigDecimal.valueOf(10.00), 0));
+        UserSortTest user5 = new UserSortTest("Иван", "Сидоров", 104);
+        user5.setAdress(new Adress("Пушкина", "Москва", "1"));
+        user5.products.add(new Price("Горох", BigDecimal.TEN, 100));
+        user5.products.add(new Price("Яблоко", BigDecimal.valueOf(10.22), 100));
+        user5.products.add(new Price("Горох", BigDecimal.TEN, 100));
+        user5.products.add(new Price("Банан", BigDecimal.valueOf(1.20), 0));
+        user5.products.add(new Price("Pickle", BigDecimal.valueOf(100.00), 100));
+        return new ArrayList<>(asList(user,user1,user2,user3,user4,user5));
+
+    }
+    @Test
+  public   void testFilterAge(){
+       List<UserSortTest> dealers= getTestDealers();
+       log.info("Список продавцов {}",dealers);
+        List<UserSortTest> result = dealers.stream().filter(v -> v.age > 104)
+            .collect(Collectors.toList());
+        assertThat(result.size()).isEqualTo(3);
+    }
+@Test
+public void testFilterName(){
+    List<UserSortTest> dealers= getTestDealers();
+    List<UserSortTest> sortedUserByName = dealers.stream().sorted(
+            Comparator.comparing(UserSortTest::getUsername).thenComparing(UserSortTest::getUsername))
+        .collect(
+            Collectors.toList());
+     sortedUserByName.forEach(r->log.info("Сортировка по имени: {}",r));
+
+    dealers = sortedUserByName.stream().map(u->new UserSortTest(u.username,
+        u.lastName,u.getAge(),u.getAdress(),
+        u.getProducts().stream()
+            .sorted(Comparator.comparing(Price::getProductName,Comparator.naturalOrder())
+                .thenComparing(Price::getCount,Comparator.reverseOrder())).collect(
+            Collectors.toList()))).collect(Collectors.toList());
+    dealers.forEach(r-> log.info("Сортировка по продукту: {} ",r));
+
+ }
+
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    private class Price {
+
+        private String productName = "";
+        private BigDecimal price = BigDecimal.ONE;
+        private Integer count = 0;
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    private class Adress {
+
+        private String street = "";
+        private String city = "";
+        private String dom = "";
+
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    private class UserSortTest {
+
+        private String username = "";
+        private String lastName = "";
+        private Integer age = 10;
+        private Adress adress = new Adress();
+        private List<Price> products = new ArrayList<>();
+
+        public UserSortTest(String username, String lastName, Integer age) {
+            this.username = username;
+            this.lastName = lastName;
+            this.age = age;
+        }
+    }
+}
